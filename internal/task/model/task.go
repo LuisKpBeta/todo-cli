40b1c654ed467e5a737c7272450df33f4fce4704
Luis Carlos <lu.ca.oliveira41@gmail.com>
2022-11-19 19:23:04 +0000

    feat: create task correctly

---
+++ internal/task/model/task.go
@@ -0,0 +1,58 @@
+package task
+
+import (
+	"errors"
+	"time"
+)
+
+type TaskPriority string
+
+const (
+	Low    TaskPriority = "low"
+	Normal TaskPriority = "normal"
+	High   TaskPriority = "high"
+)
+
+type Task struct {
+	Id          int
+	Description string
+	Status      bool
+	Priority    TaskPriority
+	Created     time.Time
+}
+
+func NewTask(id int, description string, priority TaskPriority) (*Task, error) {
+	task := &Task{
+		Id:          id,
+		Description: description,
+		Status:      false,
+		Priority:    priority,
+		Created:     time.Now(),
+	}
+	isValid := task.IsValid()
+	if isValid != nil {
+		return nil, isValid
+	}
+	return task, nil
+
+}
+func (t *Task) IsValid() error {
+	if len(t.Description) < 3 {
+		return errors.New("descption must have at least 3 characters")
+	}
+	isValisPriority := t.IsValidPriority()
+	if isValisPriority != nil {
+		return isValisPriority
+	}
+	if t.Id <= 0 {
+		return errors.New("invalid Id")
+	}
+	return nil
+}
+func (t *Task) IsValidPriority() error {
+	switch t.Priority {
+	case Low, Normal, High:
+		return nil
+	}
+	return errors.New("status must be low, high or normal")
+}
---
+++ internal/task/model/task_test.go
@@ -0,0 +1,39 @@
+package task
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"bou.ke/monkey"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	monkey.Patch(time.Now, func() time.Time {
+		return time.Date(2022, 11, 19, 12, 59, 59, 0, time.Local)
+	})
+	m.Run()
+	os.Exit(0)
+}
+
+func TestShouldCreateNewTask(t *testing.T) {
+	newTask, _ := NewTask(1, "nova task", Low)
+	assert.Equal(t, newTask.Id, 1)
+	assert.Equal(t, newTask.Description, "nova task")
+	assert.Equal(t, newTask.Priority, Low)
+	assert.Equal(t, newTask.Status, false)
+	assert.Equal(t, newTask.Created, time.Now())
+}
+func TestShouldReturnErrorOnInvalidDescription(t *testing.T) {
+	_, err := NewTask(1, "", Low)
+	assert.Error(t, err, "descption must have at least 3 characters")
+}
+func TestShouldReturnErrorOnInvalidPriority(t *testing.T) {
+	_, err := NewTask(1, "new task", "more or less")
+	assert.Error(t, err, "status must be low, high or normal")
+}
+func TestShouldReturnErrorOnInvalidId(t *testing.T) {
+	_, err := NewTask(-1, "new task", Low)
+	assert.Error(t, err, "invalid Id")
+}
