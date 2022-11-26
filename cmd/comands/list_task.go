20faa48f3b7bbaa741df32074fc18b09b3d093e2
Luis Carlos <lu.ca.oliveira41@gmail.com>
2022-11-26 22:39:42 +0000

    feat: create basic task list

--- cmd/comands/list_task.go
+++ cmd/comands/list_task.go
@@ -3,20 +3,28 @@ package comands
 import (
 	"database/sql"
 	"fmt"
+	repo "todo/internal/task/database"
+	usecases "todo/internal/task/usecases/list_tasks"
 
 	"github.com/spf13/cobra"
 )
 
 func ListTasksComand(dbConn *sql.DB) *cobra.Command {
+	listTasksUseCase := buildLisTask(dbConn)
 	comand := &cobra.Command{
 		Use:   "list [-a]",
 		Short: "show pending tasks. use -a (--all) for all tasks",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("task %s\n finished", args[0])
+		Run: func(cmd *cobra.Command, _ []string) {
+			taskList, _ := listTasksUseCase.Execute(false)
+			fmt.Println(taskList)
 		},
 	}
 	comand.DisableFlagsInUseLine = true
 	return comand
 }
 
+func buildLisTask(dbConn *sql.DB) *usecases.ListTaskUseCase {
+	repository := repo.NewTaskRepository(dbConn)
+	listTask := usecases.NewListTaskUseCase(repository)
+	return listTask
+}
