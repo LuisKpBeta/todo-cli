1a01bb3b1518cb0256881b679ce6779be0a4bf43
Luis Carlos <lu.ca.oliveira41@gmail.com>
2022-11-30 23:20:10 +0000

    refactor: create functions to execute add task

--- cmd/comands/add_task.go
+++ cmd/comands/add_task.go
@@ -13,24 +13,10 @@ import (
 )
 
 func AddTaskComand(dbConn *sql.DB) *cobra.Command {
-	addTaskUseCase := buildAddTask(dbConn)
 	comand := &cobra.Command{
 		Use:   "add <description> [-p <priority>]",
 		Short: "crete a new task with description and priority (low, normal or high)",
-		Run: func(cmd *cobra.Command, args []string) {
-			priority, _ := cmd.Flags().GetString("priority")
-			addTaskDto := dto.AddTaskDTO{
-				Description: strings.Join(args, " "),
-				Priority:    priority,
-			}
-			task, err := addTaskUseCase.Execute(addTaskDto)
-			if err != nil {
-				fmt.Println("WARNING")
-				fmt.Println(err)
-			}else {
-				fmt.Println("TASK \"",task.Description,"\" criada com sucesso")
-			}
-		},
+		Run:   runAddTask(dbConn),
 	}
 	comand.DisableFlagsInUseLine = true
 	comand.Flags().StringP("priority", "p", "", "define task priority")
@@ -41,4 +27,21 @@ func buildAddTask(dbConn *sql.DB) *usecases.AddTaskUseCase {
 	repository := repo.NewTaskRepository(dbConn)
 	addTask := usecases.NewAddTaskUseCase(repository)
 	return addTask
+}
+func runAddTask(dbConn *sql.DB) func(cmd *cobra.Command, args []string) {
+	addTaskUseCase := buildAddTask(dbConn)
+	return func(cmd *cobra.Command, args []string) {
+		priority, _ := cmd.Flags().GetString("priority")
+		addTaskDto := dto.AddTaskDTO{
+			Description: strings.Join(args, " "),
+			Priority:    priority,
+		}
+		task, err := addTaskUseCase.Execute(addTaskDto)
+		if err != nil {
+			fmt.Println("WARNING")
+			fmt.Println(err)
+		} else {
+			fmt.Println("TASK \"", task.Description, "\" criada com sucesso")
+		}
+	}
 }
