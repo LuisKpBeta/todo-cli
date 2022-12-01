8a7f3b0db1da10ccd9b7149b90e9a85c8b65c726
Luis Carlos <lu.ca.oliveira41@gmail.com>
2022-12-01 04:38:12 +0000

    feat: ensure next command returns next tasks

--- cmd/comands/next_task.go
+++ cmd/comands/next_task.go
@@ -2,7 +2,9 @@ package comands
 
 import (
 	"database/sql"
-	"fmt"
+	c "todo/cmd/common"
+	repo "todo/internal/task/database"
+	usecases "todo/internal/task/usecases/next_tasks"
 
 	"github.com/spf13/cobra"
 )
@@ -11,11 +13,23 @@ func NextTasksComand(dbConn *sql.DB) *cobra.Command {
 	comand := &cobra.Command{
 		Use:   "next",
 		Short: "show next task of each priority",
-		Run: func(cmd *cobra.Command, _ []string) {
-			fmt.Printf("next tasks")
-		},
+		Run: runListNextTask(dbConn),
 	}
 	comand.DisableFlagsInUseLine = true
 	return comand
+}
+
+func buildLisNextTask(dbConn *sql.DB) *usecases.ListNextTaskUseCase {
+	repository := repo.NewTaskRepository(dbConn)
+	listTask := usecases.NewListNextTaskUseCase(repository)
+	return listTask
+}
+func runListNextTask(dbConn *sql.DB) func(cmd *cobra.Command, args []string) {
+	listTasksUseCase := buildLisNextTask(dbConn)
+
+	return func(cmd *cobra.Command, _ []string) {
+		taskList, _ := listTasksUseCase.Execute()
+		c.PrettyListPrinter(taskList)
+	}
 }
 
