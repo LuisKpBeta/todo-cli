0036c90517785bb8619f3db2797df08ede38b607
Luis Carlos <lu.ca.oliveira41@gmail.com>
2022-12-01 04:19:58 +0000

    refactor: splitting the code into functions

--- cmd/comands/complete_task.go
+++ cmd/comands/complete_task.go
@@ -12,28 +12,11 @@ import (
 )
 
 func CompleteTaskComand(dbConn *sql.DB) *cobra.Command {
-	completeTaskUseCase := buildCompleteTask(dbConn)
 	comand := &cobra.Command{
 		Use:   "complete <id>",
 		Short: "mask a task as done",
 		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			taskId, err :=  strconv.Atoi(args[0])
-			if(err!=nil){
-				if errors.Is(err, strconv.ErrSyntax) {
-					fmt.Printf("invalid value \"%s\" for a task id\n",args[0])
-				}else{
-					fmt.Println(err.Error())
-				}
-			}else{
-				task, err:= completeTaskUseCase.Execute(int(taskId))
-				if(err!=nil){
-					fmt.Println(err.Error())
-					return
-				}
-				fmt.Println("task:", task.Id, "completed in ", task.Age())
-			}
-		},
+		Run:   runCompleteTask(dbConn),
 	}
 	comand.DisableFlagsInUseLine = true
 	return comand
@@ -42,4 +25,29 @@ func buildCompleteTask(dbConn *sql.DB) *usecases.CompleteTaskUseCase {
 	repository := repo.NewTaskRepository(dbConn)
 	completeTasl := usecases.NewCompleteTaskUseCase(repository, repository)
 	return completeTasl
+}
+func runCompleteTask(dbConn *sql.DB) func(cmd *cobra.Command, args []string) {
+	completeTaskUseCase := buildCompleteTask(dbConn)
+	return func(cmd *cobra.Command, args []string) {
+		taskId, err := strconv.Atoi(args[0])
+		if err != nil {
+			checkForError(err, args[0])
+			return
+		}
+		task, err := completeTaskUseCase.Execute(int(taskId))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println("task:", task.Id, "completed in ", task.Age())
+	}
+}
+func checkForError(err error, value string){
+	var msg string
+	if errors.Is(err, strconv.ErrSyntax) {
+		msg = fmt.Sprint("invalid value \"", value, "\" for a task id\n")
+	} else {
+		msg = err.Error()
+	}
+	fmt.Println(msg)
 }
