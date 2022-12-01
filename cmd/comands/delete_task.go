3f996839696f46d41903552b2220082673465f27
Luis Carlos <lu.ca.oliveira41@gmail.com>
2022-12-01 04:32:37 +0000

    feat: ensure delete command proceed with task deletion

--- cmd/comands/delete_task.go
+++ cmd/comands/delete_task.go
@@ -3,6 +3,9 @@ package comands
 import (
 	"database/sql"
 	"fmt"
+	common "todo/cmd/common"
+	repo "todo/internal/task/database"
+	usecases "todo/internal/task/usecases/delete_task"
 
 	"github.com/spf13/cobra"
 )
@@ -12,10 +15,30 @@ func DeleteTaskComand(dbConn *sql.DB) *cobra.Command {
 		Use:   "delete <id>",
 		Short: "delete a task",
 		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("task %s\n deleted", args[0])
-		},
+		Run: runDeleteTask(dbConn),
 	}
 	comand.DisableFlagsInUseLine = true
 	return comand
+}
+
+func buildDeleteTask(dbConn *sql.DB) *usecases.DeleteTaskUseCase {
+	repository := repo.NewTaskRepository(dbConn)
+	completeTasl := usecases.NewDeleteTaskUseCase(repository)
+	return completeTasl
+}
+func runDeleteTask(dbConn *sql.DB) func(cmd *cobra.Command, args []string) {
+	deleteTaskUseCase := buildDeleteTask(dbConn)
+	return func(cmd *cobra.Command, args []string) {
+		taskId, err := common.CheckAndParseIdArg(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = deleteTaskUseCase.Execute(int(taskId))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println("deletion succeed")
+	}
 }
